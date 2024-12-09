7654c1688c79b13f58c4fd8667c89760f63593b1
이이삭 <dlrltn1091@gmail.com>
2024-12-09 11:37:32 +0900

    Update agent.js

--- week-04-homework/routes/agent.js
+++ week-04-homework/routes/agent.js
@@ -8,8 +8,17 @@ const players = [
     {name:"호날두", speed: 100, shouting:100, grade: "s"}
   ]
 
+  //개별조회.
+router.get("/agent/:agentId", async (req, res, next) => {
+    const agentId = req.params.agentId;
+    res.send(`name: ${players[+agentId].name} ,speed: ${players[+agentId].speed} ,shouting: ${players[+agentId].shouting} ,grade: ${players[+agentId].grade}`);
+})
+
+//전체조회
 router.get("/agent", async (req, res, next) => {
-    res.send(`${players[0]}`);
+    const formattedPlayers = players.map(player => {
+        return `name: ${player.name}, speed: ${player.speed}, shouting: ${player.shouting}, grade: ${player.grade}`;
+      });
 })
 
 
